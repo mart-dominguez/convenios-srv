41467f7403c2954e8cdcc031d7832118696a0232
mdomingu <mdomingu@gmailcom>
2020-09-18 09:47:53 -0300

    inicial

---
+++ src/main/java/ar/com/sancorsalud/convenios2/ConveniosSrvApplication.java
@@ -0,0 +1,13 @@
+package ar.com.sancorsalud.convenios2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConveniosSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConveniosSrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/sancorsalud/convenios2/ConveniosSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.sancorsalud.convenios2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveniosSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
